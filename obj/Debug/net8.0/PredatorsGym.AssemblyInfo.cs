463abd2ec3093a9cfafb60d5671919d29484a17c
JosemiHk <josmipvaltp@gmail.com>
2025-07-01 17:05:56 -0500

    fix-nombre de la app cambiado
    Se realizó el cambio en base a lo solicitado por el Product Owner

--- obj/Debug/net8.0/PredatorsGym.AssemblyInfo.cs
+++ obj/Debug/net8.0/PredatorsGym.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PredatorsGym")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7affc03f7dfe14458b1b3c755b7215f9431c1bba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcb41a2ceb44a757798b3ab6d7c0ce9a909dc913")]
 [assembly: System.Reflection.AssemblyProductAttribute("PredatorsGym")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PredatorsGym")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
