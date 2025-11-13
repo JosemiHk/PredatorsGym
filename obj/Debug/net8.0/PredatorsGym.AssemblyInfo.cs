73dc0cbdc2699a8223ebfb23cb62dae08a302d5d
JosemiHk <josmipvaltp@gmail.com>
2025-11-13 14:10:40 -0500

    Render Markdown in RutinaGenerada view with Markdig
    Added Markdig package and integrated Markdown-to-HTML rendering for the RutinaGenerada view. Updated UI to display generated routines with improved styling and accessibility, and adjusted icon markup for better ARIA support.

--- obj/Debug/net8.0/PredatorsGym.AssemblyInfo.cs
+++ obj/Debug/net8.0/PredatorsGym.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PredatorsGym")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7c000c38a7e8364d2a1928c4870d2fb57d2bb14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d22efbe6d3a75707abc204b728ad4f193f716be2")]
 [assembly: System.Reflection.AssemblyProductAttribute("PredatorsGym")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PredatorsGym")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
