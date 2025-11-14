f51e9e7a20cd4882a8019c96e2e947b554ac71d1
JosemiHk <josmipvaltp@gmail.com>
2025-11-13 22:15:39 -0500

    Limit Telefono field length in PerfilesUsuarios
    Added a migration to change the 'Telefono' column in the PerfilesUsuarios table from nvarchar(max) to nvarchar(15) with a max length of 15 characters. Updated model snapshot and generated migration files accordingly.

--- Migrations/20251113211246_Nutricion123.Designer.cs
+++ Migrations/20251113211246_Nutricion123.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PredatorsGym.Datos;
 
@@ -11,9 +12,11 @@ using PredatorsGym.Datos;
 namespace PredatorsGym.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251113211246_Nutricion123")]
+    partial class Nutricion123
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20251113211246_Nutricion123.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PredatorsGym.Migrations
+{
+    /// <inheritdoc />
+    public partial class Nutricion123 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Telefono",
+                table: "PerfilesUsuarios",
+                type: "nvarchar(15)",
+                maxLength: 15,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Telefono",
+                table: "PerfilesUsuarios",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(15)",
+                oldMaxLength: 15,
+                oldNullable: true);
+        }
+    }
+}
